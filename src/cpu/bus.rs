7a73bf45769c5c42083503fe6c5361f513a99756
JaydenElliott <jayden.elliott@outlook.com>
2021-03-24 08:04:42 +1100

    BUILD: bus instantiation.

---
+++ src/cpu/bus.rs
@@ -0,0 +1,30 @@
+
+
+pub struct Bus {
+    cpu: cpu
+}
+
+impl Bus {
+    fn new() -> Self {
+        Self {cpu: cpu::new()}
+    }
+}
+
+//
+// class Bus
+// {
+// public:
+// Bus();
+// ~Bus();
+//
+// public: // Devices on bus
+// olc6502 cpu;
+//
+// // Fake RAM for this part of the series
+// std::array<uint8_t, 64 * 1024> ram;
+//
+//
+// public: // Bus Read & Write
+// void write(uint16_t addr, uint8_t data);
+// uint8_t read(uint16_t addr, bool bReadOnly = false);
+// };
