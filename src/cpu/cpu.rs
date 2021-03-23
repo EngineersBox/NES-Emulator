f08d8774c2126303f738aff23d364dfeac7f524a
JaydenElliott <jayden.elliott@outlook.com>
2021-03-24 08:32:36 +1100

    BUILD: cpu reset registers, program counter and stack pointer.

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,10 +1,12 @@
+
+
 #[derive(Debug)]
 struct Registers {
     a: u8,      // Accumulator
     x: u8,      // X register
     y: u8,      // Y register
     pc: u16,     // Program Counter
-    stkp: u8,   // Stack pointer register
+    sp: u8,   // Stack pointer register
     status: u8, // Status register
 }
 
@@ -15,8 +17,34 @@ pub struct CPU {
 // CPU methods
 impl CPU {
     // Constructor
-    fn new() -> Self {
+    pub fn new() -> Self {
         // Initialise registers
-        Self { registers: Registers { a: 0x00, x: 0x00, y: 0x00, pc: 0x0000, stkp: 0x00, status: 0x00}}
+        Self { registers: Registers { a: 0x00, x: 0x00, y: 0x00, pc: 0x0000, sp: 0x00, status: 0x00}}
+    }
+
+    // Reset CPU to default state
+    // Registers set as per:
+    // https://www.c64-wiki.com/wiki/Reset_(Process)
+
+    pub fn reset(&mut self) -> (){
+
+        // Reset registers (Cycle 0)
+        self.registers.a = 0x00;
+        self.registers.x = 0x00;
+        self.registers.y = 0x00;
+        self.registers.sp = 0xFD;
+
+
+        // Set program counter by reading from reset vector (0xFFFD - 0xFFFC)
+        self.registers.pc = (bus::read(0xFFFD) << 8) + bus::read(0xFFFC);
+
+        // Set status flags
+
+
+
+
+
+
+
     }
 }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,3 +1,6 @@
 pub mod opcode_compression;
 pub mod cpu;
 pub mod flags;
+pub mod opcodes;
+pub mod bus;
+pub mod utils;
