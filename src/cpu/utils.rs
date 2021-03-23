085614b8ee389b7c980267b3832a398ddfff5bbb
JaydenElliott <jayden.elliott@outlook.com>
2021-03-24 08:09:36 +1100

    BUILD: hex address range check utility function.

---
+++ src/cpu/utils.rs
@@ -0,0 +1,8 @@
+// Verifies if hex address in a legal range.
+pub fn check_hex_range(addr: u16) -> bool{
+    return if addr < 0x0000 || addr > 0xFFFF {
+        false
+    } else {
+        true
+    }
+}
